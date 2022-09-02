014f80993316f100b7a3c3b38abaf399ffd8918d
leonardo Barbosa Braga <barbosaleonardo466@gmail.com>
2022-09-02 14:51:57 -0300

    pagina do facebook com poucas alterações para teste

---
+++ src/Script.js
@@ -0,0 +1,15 @@
+function showOcultPassword() {
+    var Password = document.getElementById("PasswordID");
+    if(Password.type=="password"){
+        Password.type="text";
+    } else {
+        Password.type="password";
+    }
+}
+
+function ShowGender() {
+    var gender_Custom = document.getElementById("gender_C")
+
+    if (gender_Custom = true ){  
+    } 
+}
